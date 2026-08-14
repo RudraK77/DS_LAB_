8d784ad31a6bbf55f1b87b153a3131f32a147479
RudraK77 <rudrakulkarni2121@gmail.com>
2026-08-14 11:43:25 +0530

    till singly linked list

---
+++ DSLAB1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+int main() {
+    string exp = "231*+9-";
+    stack<int> st;
+
+    for (char ch : exp) {
+        if (isdigit(ch)) {
+            st.push(ch - '0');
+        }
+        else {
+            int b = st.top(); st.pop();
+            int a = st.top(); st.pop();
+
+            switch (ch) {
+                case '+': st.push(a + b); break;
+                case '-': st.push(a - b); break;
+                case '*': st.push(a * b); break;
+                case '/': st.push(a / b); break;
+            }
+        }
+    }
+
+    cout << "Result = " << st.top();
+
+    return 0;
+}
